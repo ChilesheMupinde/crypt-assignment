b126a611c12eaedd528a31ddc373ebbe13973ceb
ChilesheMupinde <115735605+ChilesheMupinde@users.noreply.github.com>
2024-03-29 12:22:02 +0200

    Add files via upload

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,9 +1,10 @@
 
-const String ip = "";
+const String ip = " 192.168.153.245";
 const String port = "7545";
-final String rpcUrl = "http://$ip:$port";
-final String WebsocketUrl = "ws://$ip:$port";
+final String rpcUrl = "HTTP://127.0.0.1:7545";
+final String WebsocketUrl = "ws://127.0.0.1:7545";
 final String funder_privatekey = "0xF0354f2aA5f211d61fC5961A7486dEe31D6d8936";
-final String privatekey = "0x76240b0C392C79B2061be1611AD19c3b33Cf1Ef8";
+final String privatekey
+= "0x26437b9ee64978efc59124cd86410d7f871cf3be2fa521874e6705472e3c27ca";
 
-String ContractAddress1 = "0xDEeDfdcAc72346887E95462156f5205e9e4d6Cf3";
+String ContractAddress1 = "0x5E8d2c27e37D9D9e4F6C8E3A89AC3b575C26217C";
