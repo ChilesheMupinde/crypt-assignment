35e96e806a085f517ce1cbfa7007e348a14601e0
ChilesheMupinde <115735605+ChilesheMupinde@users.noreply.github.com>
2024-03-29 12:21:21 +0200

    Add files via upload

---
+++ lib/models/campaign_model.dart
@@ -0,0 +1,41 @@
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+import 'dart:core';
+
+import 'package:flutter/cupertino.dart';
+import 'package:web3dart/web3dart.dart';
+
+// class Campaign {
+//   Web3Client User;
+//   String title;
+//   String Description;
+//   int targetAmount;
+//   DateTime deadline;
+//
+//   Campaign({
+//     required this.User,
+//     required this.title,
+//     required this.Description,
+//     required this.targetAmount,
+//     required this.deadline,
+//
+//
+//   });
+//
+//   Campaign.fromJson(Map<String, dynamic> json)
+//       :
+//         title = json['title'],
+//         Description = json['Description'],
+//         targetAmount = json['targetAmount'],
+//         deadline = json['deadline'],
+//         User = json['User'];
+//
+//   Map<String, dynamic> toJson() => {
+//     "title": this.title,
+//     "Description": this.Description,
+//     "targetAmount": this.targetAmount,
+//     "deadline": this.deadline,
+//     "User": this.User
+//   };
+//
+//
+// }
